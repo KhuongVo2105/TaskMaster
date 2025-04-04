ea5d160d6db1a458ee3e55dd32904d8fc7824742
KhuongVo2105 <khuongvo2105@gmail.com>
2025-04-04 17:03:16 +0700

    push commit without env file on gitignore

--- TaskMaster/src/main/java/com/taskmaster/TaskMaster/Entities/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package com.taskmaster.TaskMaster.Entities;
-
-public enum TaskStatus {
-    PENDING,      // Chưa bắt đầu
-    IN_PROGRESS,  // Đang thực hiện
-    DONE,         // Hoàn thành
-    CANCELLED     // Đã hủy
-}
